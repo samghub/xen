834af09bf832aa607bc6fe59e1e7d0367d01a875
Tamas K Lengyel <tamas@tklengyel.com>
2016-05-03 13:29:32 -0600

    tools/libxc: add xc_monitor_privileged_call
    These are the user-space components for the new ARM SMC events.
    
    Signed-off-by: Tamas K Lengyel <tamas@tklengyel.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>
    ---
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/libxc/include/xenctrl.h
+++ tools/libxc/include/xenctrl.h
@@ -2160,6 +2160,8 @@ int xc_monitor_software_breakpoint(xc_interface *xch, domid_t domain_id,
                                    bool enable);
 int xc_monitor_guest_request(xc_interface *xch, domid_t domain_id,
                              bool enable, bool sync);
+int xc_monitor_privileged_call(xc_interface *xch, domid_t domain_id,
+                               bool enable);
 
 /**
  * This function enables / disables emulation for each REP for a
